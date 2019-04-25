5e67301c9a8664dc2d0dce45fde7252bc1963855
guot123 <2268175061@qq.com>
2019-04-25 09:09:16 +0800

    日考

---
+++ day12/Sum.php
@@ -0,0 +1,13 @@
+<?php
+class Sum
+{
+    public static function Sum1($sum2,$sum3){
+        $arr = array($sum2);
+        $arr1 = array($sum3);
+        $sum = array_merge($arr,$arr1);
+        
+        return array_sum($sum);
+    }
+}
+
+?>
---
+++ day12/index.php
@@ -0,0 +1,8 @@
+<?php
+include 'Sum.php';
+
+$sum = Sum::Sum1(2,6);
+print_r($sum);
+
+
+?>
